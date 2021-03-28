9efd1e1c26d87a90721a0ef1dbfca82a5dd3bd9a
LRezende <lucas.rezende.2000@gmail.com>
2021-03-28 19:31:04 -0300

    Tela de Sorteio Completa

--- src/pages/Raffle/index.jsx
+++ src/pages/Raffle/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from './styles';
+import Text from '../../components/Text';
+
+import { Container, Logo, RaffledItemContainer } from './styles';
+
+import logo from '../../assets/logo.png';
 
 export default function Raffle() {
   return (
     <Container>
-      <h1>Raffle</h1>
+      <Logo src={logo} />
+      <Text size={50} fontFamily="Lora" style={{ fontWeight: 700 }}>OBRIGADO</Text>
+      <Text size={40} fontFamily="Lora" style={{ fontWeight: 700, textAlign: 'center' }}>O presente sorteado foi:</Text>
+      <RaffledItemContainer>
+        <Text size={45} fontFamily="Lora" style={{ fontWeight: 700, textAlign: 'center', color: 'var(--color-bright-black)', margin: 0 }}>Jogo de Panelas</Text>
+      </RaffledItemContainer>
     </Container>
   );
 }
--- src/pages/Raffle/styles.js
+++ src/pages/Raffle/styles.js
@@ -4,6 +4,22 @@ export const Container = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  background-color: var(--color-primary);
+`;
+
+export const Logo = styled.img`
+  height: 130px;
+  width: 130px;
+`;
+
+export const RaffledItemContainer = styled.div`
+  width: 100%;
+  max-width: 550px;
+  background-color: var(--color-secondary);
+  border-radius: 10px;
+  margin-top: 50px;
+  padding: 20px 10px;
 `;
